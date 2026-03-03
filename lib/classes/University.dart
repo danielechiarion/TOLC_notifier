cdcc26aa871b8a2c4bb5df1b2ec250effbc3e282
docente1 <docente1@internal.iisviolamarchesini.edu.it>
2026-03-03 12:27:20 +0100

    completed University class

--- lib/classes/University.dart
+++ lib/classes/University.dart
@@ -7,5 +7,19 @@ class University{
   /// Constructor method for the University
   University(this.name);
 
-  
+  /// Converter to map object for sql requests
+  Map<String, dynamic> toMap() => {'name':name};
+
+  /// Converter from Map Object to University object
+  factory University.fromMap(Map<String, dynamic> map) => University(map['name']);
+
+  /// Override of equal method for the same name
+  @override
+  bool operator ==(Object other){
+    return other is University && other.name == name;
+  }
+
+  /// Override of hash method
+  @override
+  int get hashCode => name.hashCode;
 }
