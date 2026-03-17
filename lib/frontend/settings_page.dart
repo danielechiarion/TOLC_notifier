5c7bc1a790b0ffdb7d23743c9adc1ec2d6451011
docente1 <docente1@internal.iisviolamarchesini.edu.it>
2026-03-17 11:40:01 +0100

    starting new section for settings customization

---
+++ lib/frontend/settings_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// Class to manange settings 
+/// and change them during the execution of the application
+class SettingsPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+
+  /* function to control the existance of 
+  eventual variables on storage and assign them default values */
+  void _checkLocalVariables(){
+    
+  }
+}
