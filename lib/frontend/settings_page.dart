bb6fae930aaf5f8ca5a18aff0269ffb792138476
docente1 <docente1@internal.iisviolamarchesini.edu.it>
2026-03-17 12:42:42 +0100

    add method to check local variables static in the settings class

--- lib/frontend/settings_page.dart
+++ lib/frontend/settings_page.dart
@@ -6,12 +6,22 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SettingsPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+    
+
     return Scaffold();
   }
 
   /* function to control the existance of 
   eventual variables on storage and assign them default values */
-  void _checkLocalVariables(){
-    
+  static void checkLocalVariables() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance(); // get the shared preference
+    /* check if variables for enabling or disabling
+    actions are set, or create it setting them by default */
+    if(localStorage.get('search_enabled') == null){
+      localStorage.setBool('search_enabled', true);
+    } 
+    if(localStorage.get('notify_enabled') == null){
+      localStorage.setBool('notify_enabled', true);
+    }
   }
 }
