1a02d4ee1e821410faba3bd211a0e2085535b0a3
danielechiarionù <daniele.chiarion@iisviolamarchesini.edu.it>
2026-04-03 11:27:48 +0200

    add logging output to a file

--- lib/services/logger_utils.dart
+++ lib/services/logger_utils.dart
@@ -1,4 +1,20 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:logger/logger.dart';
+
+/// Use class for the export of logs to
+/// a file in the device's storage.
+class FileOutput extends LogOutput {
+  @override
+  void output(OutputEvent event) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/app_logs.log');
+    
+    for (var line in event.lines) {
+      await file.writeAsString('$line\n', mode: FileMode.append);
+    }
+  }
+}
 
 /* define the instance of the logger */
 final logger = Logger(
@@ -6,4 +22,10 @@ final logger = Logger(
     colors: true,
     printEmojis: true,
   ),
+  output: MultiOutput(
+    [
+      ConsoleOutput(),
+      FileOutput()
+    ]
+  )
 );
